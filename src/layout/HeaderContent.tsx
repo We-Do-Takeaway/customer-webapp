c70c0b7e754d5e00a1977f64147a0e7bb6757ad8
Zac Tolley <zac@thetolleys.com>
2020-09-29 12:04:20 +0100

    Create some simple layouts to prove antd works

---
+++ src/layout/HeaderContent.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { Layout, Menu, Breadcrumb } from 'antd'
+
+import styles from './HeaderContent.module.css'
+
+const { Header, Content, Footer } = Layout
+
+export const HeaderContent: React.FC = ({ children }) => (
+  <Layout className={styles.layout}>
+    <Header>
+      <div className={styles.logo} />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className={styles.siteLayoutContent}>{children}</div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Footer</Footer>
+  </Layout>
+)
