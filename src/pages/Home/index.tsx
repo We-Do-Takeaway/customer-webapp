67656a63a0f543a4bfa25ecfdaf2e0f5e4d8ed90
Zac Tolley <zac@thetolleys.com>
2020-09-30 15:57:09 +0100

    Move layout to layouts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { HeaderContent } from '../../layout'
+import { HeaderContent } from '../../layouts'
 
 export const Home: React.FC = () => (
   <HeaderContent>Welcome to We Do Takeaway</HeaderContent>
