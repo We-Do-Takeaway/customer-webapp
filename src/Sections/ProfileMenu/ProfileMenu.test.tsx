2f7386c9ea052f7f8992ddb231164998e1981d71
Zac Tolley <zac@thetolleys.com>
2020-10-13 10:17:51 +0100

    Move profile section to own folder

--- src/Sections/ProfileMenu/ProfileMenu.test.tsx
+++ src/Sections/ProfileMenu/ProfileMenu.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, RenderResult } from '@testing-library/react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-import { UserContext, UserType } from '../contexts'
+import { UserContext, UserType } from '../../contexts'
 import { ProfileMenu } from '.'
 
 describe('Profile Menu', () => {
