d3bbc148bc4f18fcfcca27e2c8e027e531bcae56
Zac Tolley <zac@thetolleys.com>
2020-10-18 17:30:52 +0100

    Loosen the lint rules a little
    allow underscores in variables to allow names to match jwt names
    Also allow empty functions for easier mocking and no-use-before-define due to how some code needs to work

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,9 @@ module.exports = {
   },
   rules: {
     'react/jsx-wrap-multilines': 0,
+    'react-hooks/exhaustive-deps': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-use-before-define': 0,
   },
 }
