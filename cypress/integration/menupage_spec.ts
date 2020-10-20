be3acded45b11204fc4ac4b48337cf83e44bf1b3
Zac Tolley <zac@thetolleys.com>
2020-10-20 12:36:13 +0100

    Improve menu page

---
+++ cypress/integration/menupage_spec.ts
@@ -0,0 +1,132 @@
+const sectionSel = (sectionNumber: number) =>
+  `[data-testid="section-list-section"]:nth-child(${sectionNumber})`
+const sectionItemSel = (sectionNumber: number, itemNumber: number) =>
+  `[data-testid="section-list-section"]:nth-child(${sectionNumber}) [data-testid="section-items-item"]:nth-child(${itemNumber})`
+
+context('Menu page', () => {
+  before(() => {
+    cy.login()
+    cy.visit('/menu/600dca30-c6e2-4035-ad15-783c122d6ea4')
+  })
+
+  it('display menu details', () => {
+    cy.get('h2[data-testid="menu-detail-card-title"]')
+      .first()
+      .should('contain', 'Cafe comfort and waffles')
+
+    cy.get('p[data-testid="menu-detail-card-description"]')
+      .first()
+      .should(
+        'contain',
+        "The cafe is all about food to make you feel better when you can't face the world or feel like you are going crazy. Have an all day breakfast, your mothers Sunday roast or help yourself to the biggest bowl of ice-cream will lots of extras"
+      )
+  })
+
+  it('display sections', () => {
+    cy.get('[data-testid="section-list"]').should('be.visible')
+    cy.get('[data-testid="section-list-section"]').should('have.length', 2)
+  })
+
+  describe('Sections', () => {
+    describe('Main menu', () => {
+      it('show the main section title and description first', () => {
+        cy.get(sectionSel(1)).within(() => {
+          cy.get('[data-testid="section-title"]').should('contain', 'Main')
+          cy.get('[data-testid="section-description"]').should(
+            'contain',
+            'Stuff to fill you up'
+          )
+        })
+      })
+
+      it('has one item in the main section', () => {
+        cy.get(`${sectionSel(1)} [data-testid="item-card"]`).should(
+          'have.length',
+          1
+        )
+      })
+
+      it('first main section item is a bowl of sausages', () => {
+        cy.get(sectionItemSel(1, 1)).within(() => {
+          cy.get('[data-testid="item-card-title"]').should(
+            'contain',
+            'Plate of sausages'
+          )
+          cy.get('[data-testid="item-card-description"]').should(
+            'contain',
+            'Big bowl of sausages'
+          )
+
+          cy.get('[data-testid="item-card-image"]')
+            .should('have.css', 'background-image')
+            .and(
+              'contain',
+              'url("https://www.wedotakeaway.com/images/sausages.jpg")'
+            )
+        })
+      })
+    })
+
+    describe('Desert', () => {
+      it('show the desert section title and description second', () => {
+        cy.get(sectionSel(2)).within(() => {
+          cy.get('[data-testid="section-title"]').should('contain', 'Desert')
+
+          cy.get('[data-testid="section-description"]').should(
+            'contain',
+            'The best part of the menu'
+          )
+        })
+      })
+
+      it('has 2 items in the desert section', () => {
+        cy.get(`${sectionSel(2)} [data-testid="item-card"]`).should(
+          'have.length',
+          2
+        )
+      })
+
+      it('first desert item is Chocolate ice-cream surprise', () => {
+        cy.get(sectionItemSel(2, 1)).within(() => {
+          cy.get('[data-testid="item-card-title"]').should(
+            'contain',
+            'Chocolate ice-cream surprise'
+          )
+
+          cy.get('[data-testid="item-card-description"]').should(
+            'contain',
+            'An amazing mixture of chocolate and cherries'
+          )
+
+          cy.get('[data-testid="item-card-image"]')
+            .should('have.css', 'background-image')
+            .and(
+              'contain',
+              'url("https://www.wedotakeaway.com/images/choc-ice-cream.webp")'
+            )
+        })
+      })
+
+      it('2nd  desert item is a bowl of cherries', () => {
+        cy.get(sectionItemSel(2, 2)).within(() => {
+          cy.get('[data-testid="item-card-title"]').should(
+            'contain',
+            'Bowl of cherries'
+          )
+
+          cy.get('[data-testid="item-card-description"]').should(
+            'contain',
+            'Big bowl of cherries'
+          )
+
+          cy.get('[data-testid="item-card-image"]')
+            .should('have.css', 'background-image')
+            .and(
+              'contain',
+              'url("https://www.wedotakeaway.com/images/cherries.jpg")'
+            )
+        })
+      })
+    })
+  })
+})
--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -1,76 +1,34 @@
 import { Link } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
 import React from 'react'
 import { Link as RouterLink, useParams } from 'react-router-dom'
 
-import { HeaderContent } from '../layouts'
+import { MenuDetailCard } from '../components'
 import { useMenu } from '../graphql/hooks'
-import { ItemCard } from '../components'
+import { HeaderContent } from '../layouts'
+import { SectionList } from '../Sections'
 
 interface MenuParams {
   menuId: string
 }
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-  items: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    flexWrap: 'wrap',
-    listStyleType: 'none',
-    margin: 0,
-    padding: 0,
-  },
-  item: {
-    flex: '0 1 44%',
-    marginBottom: 16,
-    border: '1px solid transparent',
-    borderRadius: 2,
-  },
-})
+const BreadCrumb = () => (
+  <>
+    <Link component={RouterLink} to="/" color="inherit">
+      Menus
+    </Link>
+  </>
+)
 
 export const MenuPage: React.FC = () => {
   const { menuId } = useParams<MenuParams>()
   const { loading, error, menu } = useMenu(menuId)
-  const classes = useStyles()
-
-  const BreadCrumb = () => (
-    <>
-      <Link component={RouterLink} to="/" color="inherit">
-        Menus
-      </Link>
-    </>
-  )
 
   return (
     <HeaderContent breadcrumbs={<BreadCrumb />}>
-      <header>
-        <h1>{menu?.name}</h1>
-        <p>{menu?.description}</p>
-        <p>{menu?.introduction}</p>
-      </header>
       {loading && <p>Loading...</p>}
       {error && <p>{error?.message}</p>}
-      {menu &&
-        menu?.sections?.map((section) => (
-          <React.Fragment key={section.id}>
-            <h2>{section.name}</h2>
-            <p>{section.description}</p>
-            <ul className={classes.items}>
-              {section.items?.map((item) => (
-                <li className={classes.item} key={item.id}>
-                  <ItemCard item={item} />
-                </li>
-              ))}
-            </ul>
-          </React.Fragment>
-        ))}
+      <header>{menu && <MenuDetailCard menu={menu} />}</header>
+      {menu && menu.sections && <SectionList sections={menu.sections} />}
     </HeaderContent>
   )
 }
