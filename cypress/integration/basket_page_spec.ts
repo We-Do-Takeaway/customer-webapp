c4aca1c443ed937060f120ed990c2de706988d78
Zac Tolley <zac@thetolleys.com>
2020-12-08 13:47:56 +0000

    Update integration tests to test removing basket item

--- cypress/integration/basket_page_spec.ts
+++ cypress/integration/basket_page_spec.ts
@@ -1,5 +1,5 @@
 describe('Basket items page', () => {
-  context('When an item is added to the basket', () => {
+  context('an item is added to the basket', () => {
     beforeEach(() => {
       const firstSection = '[data-testid="section-list-section"]:first-child'
       const firstItem = '[data-testid="section-items-item"]:first-child'
@@ -14,7 +14,7 @@ describe('Basket items page', () => {
       ).click()
     })
 
-    context('And the user chooses to view the basket', () => {
+    context('the user chooses to view the basket', () => {
       beforeEach(() => {
         cy.get('[data-testid="basket-indicator"] button').click()
       })
@@ -41,6 +41,68 @@ describe('Basket items page', () => {
         cy.get(
           '[data-testid="basket-page-item-table"] tbody tr:first-child td:nth-child(3)'
         ).contains('1')
+      })
+
+      context('the user chooses to delete an item', () => {
+        beforeEach(() => {
+          cy.get(
+            '[data-testid="basket-page-item-table"] tbody tr:first-child [data-testid="basket-page-remove-item"]'
+          ).click()
+        })
+
+        it('display a confirmation dialog', () => {
+          cy.get('[data-testid="delete-basket-item-confirmation"]').should(
+            'exist'
+          )
+
+          cy.get('[data-testid="delete-item-dialog-title"]').contains(
+            'Delete item from basket'
+          )
+
+          cy.get('[data-testid="delete-item-dialog-description"]').contains(
+            'Are you sure you want to remove ‘Plate of sausages’ item from the basket?'
+          )
+        })
+
+        context('press No', () => {
+          beforeEach(() => {
+            cy.get('[data-testid="delete-item-dialog-cancel"]').click()
+          })
+
+          it('close the dialog', () => {
+            cy.get('[data-testid="delete-basket-item-confirmation"]').should(
+              'not.exist'
+            )
+          })
+
+          it('do not delete the item', () => {
+            cy.get('[data-testid="basket-page-item-table"]').should('exist')
+            cy.get(
+              '[data-testid="basket-page-item-table"] tbody tr:first-child td:first-child img'
+            )
+              .should('have.attr', 'src')
+              .should(
+                'include',
+                'https://www.wedotakeaway.com/images/sausages.jpg'
+              )
+          })
+        })
+
+        context('press Yes', () => {
+          beforeEach(() => {
+            cy.get('[data-testid="delete-item-dialog-confirm"]').click()
+          })
+
+          it('close the dialog', () => {
+            cy.get('[data-testid="delete-basket-item-confirmation"]').should(
+              'not.exist'
+            )
+          })
+
+          it('delete the item', () => {
+            cy.get('[data-testid="basket-page-item-table"]').should('not.exist')
+          })
+        })
       })
     })
   })
